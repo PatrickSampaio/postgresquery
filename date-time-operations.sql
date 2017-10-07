5ef7d7fd539629ff33d73c550ac89e431e155536
PatrickSampaio <patricksampaio48@gmail.com>
2017-10-07 20:02:22 -0300

    Date time operations

---
+++ date-time-operations.sql
@@ -0,0 +1,19 @@
+﻿SELECT 
+	return_date - rental_date as "Tempo de alocação"
+FROM
+	rental;
+--DAYS
+SELECT
+	return_date - rental_date as "Tempo de alocação"
+FROM
+	rental
+WHERE
+	EXTRACT(days FROM return_date - rental_date) > 3;
+
+--HOURS
+SELECT
+	return_date - rental_date as "Tempo de alocação"
+FROM
+	rental
+WHERE
+	EXTRACT(epoch FROM return_date - rental_date)/3600 > 100;
