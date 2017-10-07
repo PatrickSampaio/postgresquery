0a6cec421146b98412b9f0477c4b360dcefbc52a
PatrickSampaio <patricksampaio48@gmail.com>
2017-10-07 19:51:31 -0300

    Postgres commands

---
+++ string-commands.sql
@@ -0,0 +1,24 @@
+﻿--LEFT FUNCTION
+SELECT  first_name,
+        last_name,
+	CONCAT(LEFT(first_name, 1), LEFT(last_name, 1)) AS "Initials"
+  FROM public.actor;
+
+--LENGTH FUNCTION
+SELECT first_name
+FROM
+	public.actor
+WHERE
+	LENGTH(first_name) > 4
+
+--CAPITALIZE
+SELECT first_name,
+	UPPER(last_name)
+FROM
+	public.actor
+
+--REPLACE
+SELECT 
+	REPLACE(first_name, 'a', '@')
+FROM
+	public.actor
